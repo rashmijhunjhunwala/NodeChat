35cd09c542cf7c5f672e9f5891c6b4ff6264678f
rashmijhunjhunwala <rashmijhunjhunwala068@gmail.com>
2019-07-15 23:48:05 +0530

    debugging

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const socketIO = require('socket.io');
 const {generateMessage, generateLocationMessage} = require('./utils/message');
 const {isRealString} = require('./utils/validation');
 const {Users} = require('./utils/users');
-
+const favicon = require('express-favicon');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -14,7 +14,7 @@ var io = socketIO(server);
 var users = new Users();
 
 app.use(express.static(publicPath));
-
+app.use(favicon(__dirname + '/public/favicon.png'));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
@@ -60,7 +60,6 @@ io.on('connection', (socket) => {
     }
   });
 });
-exports.app = functions.http.onRequest(app);
 server.listen(port, () => {
   console.log(`Server is up on ${port}`);
 });
